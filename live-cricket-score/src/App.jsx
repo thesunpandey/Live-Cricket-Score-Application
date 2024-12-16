0ff665f43bd61d1e6db692a40ec68e7254d3b91a
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-12-16 16:51:08 +0530

    Created Live Cricket Score Tracker App

---
+++ live-cricket-score/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react'
+import CricketScore from './components/CricketScore'
+
+const App = () => {
+  return (
+    <div>
+      <CricketScore />
+    </div>
+  )
+}
+
+export default App
